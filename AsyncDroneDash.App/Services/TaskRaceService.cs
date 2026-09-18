4cad1fb407b6fb5537770709d1ef5d651a13156f
rmk062026 <rmk062026@gmail.com>
2026-09-18 13:43:18 +0200

    Add concurrent drone tasks with WhenAll

--- AsyncDroneDash.App/Services/TaskRaceService.cs
+++ AsyncDroneDash.App/Services/TaskRaceService.cs
@@ -12,15 +12,75 @@ public class TaskRaceService
             MaxCheckpoints = 5,
             DelayMs = 500
         };
+
+        DroneModel bravo = new DroneModel
+        {
+            Name = "Bravo",
+            MaxCheckpoints = 5,
+            DelayMs = 200
+        };
+
+        DroneModel charlie = new DroneModel
+        {
+            Name = "Charlie",
+            MaxCheckpoints = 5,
+            DelayMs = 700
+        };
+
+        DroneModel delta = new DroneModel
+        {
+            Name = "Delta",
+            MaxCheckpoints = 5,
+            DelayMs = 400
+        };
+
         TaskCompletionSource alphaCompletion = new TaskCompletionSource();
+        TaskCompletionSource bravoCompletion = new TaskCompletionSource();
+        TaskCompletionSource charlieCompletion = new TaskCompletionSource();
+        TaskCompletionSource deltaCompletion = new TaskCompletionSource();
+
         Task alphaTask = Task.Run(() =>
         {
             FlyDrone(alpha);
             alphaCompletion.SetResult();
         });
+
+        Task bravoTask = Task.Run(() =>
+        {
+            FlyDrone(bravo);
+            bravoCompletion.SetResult();
+        });
+
+        Task charlieTask = Task.Run(() =>
+        {
+            FlyDrone(charlie);
+            charlieCompletion.SetResult();
+        });
+
+        Task deltaTask = Task.Run(() =>
+        {
+            FlyDrone(delta);
+            deltaCompletion.SetResult();
+        });
         Console.WriteLine($"alphaTask status: {alphaTask.Status}");
-        Console.WriteLine($"tcs.Task status: {alphaCompletion.Task.Status}");
-        await alphaCompletion.Task;
+        Console.WriteLine($"alphaCompletion.Task status: {alphaCompletion.Task.Status}");
+
+        Console.WriteLine($"bravoTask status: {bravoTask.Status}");
+        Console.WriteLine($"bravoCompletion.Task status:: {bravoCompletion.Task.Status}");
+
+        Console.WriteLine($"charlieTask status: {charlieTask.Status}");
+        Console.WriteLine($"charlieCompletion.Task status: {charlieCompletion.Task.Status}");
+
+        Console.WriteLine($"deltaTask status: {deltaTask.Status}");
+        Console.WriteLine($"deltaCompletion.Task status: {deltaCompletion.Task.Status}");
+
+        await Task.WhenAll(
+            alphaCompletion.Task,
+            bravoCompletion.Task,
+            charlieCompletion.Task,
+            deltaCompletion.Task
+        );
+        Console.WriteLine("Alle droner er ferdige!");
     }
 
     public void FlyDrone(DroneModel drone)
