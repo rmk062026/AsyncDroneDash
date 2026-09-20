75866099aa7f8b31ad4dc2465e31abf29ce9f338
rmk062026 <rmk062026@gmail.com>
2026-09-20 15:44:19 +0200

    Add async await done race

--- AsyncDroneDash.App/Program.cs
+++ AsyncDroneDash.App/Program.cs
@@ -2,6 +2,8 @@
 
 ThreadRaceService threadRaceService = new ThreadRaceService();
 TaskRaceService taskRaceService = new TaskRaceService();
+AsyncRaceService asyncRaceService = new AsyncRaceService();
 
 // threadRaceService.Run();
-await taskRaceService.Run();
+// await taskRaceService.Run();
+await asyncRaceService.Run();
---
+++ AsyncDroneDash.App/Services/AsyncRaceService.cs
@@ -0,0 +1,74 @@
+using AsyncDroneDash.App.Models;
+
+namespace AsyncDroneDash.App.Services;
+
+public class AsyncRaceService
+{
+    public async Task FlyDroneAsync(DroneModel drone)
+    {
+        Console.WriteLine($"{drone.Name} starter...");
+
+        for (int i = 0; i <= drone.MaxCheckpoints; i++)
+        {
+            await Task.Delay(drone.DelayMs);
+            if (drone.Name == "Alpha" && i == 3)
+            {
+                throw new Exception("Alpha fikk en feil ved checkpoint 3!");
+            }
+
+            Console.WriteLine($"{drone.Name} har kommet til checkpoint {i} og den brukte {drone.DelayMs} ms.");
+        }
+        Console.WriteLine($"{drone.Name} er ferdig");
+    }
+
+    public async Task Run()
+    {
+        DroneModel alpha = new DroneModel
+        {
+            Name = "Alpha",
+            MaxCheckpoints = 5,
+            DelayMs = 500
+        };
+
+        DroneModel bravo = new DroneModel
+        {
+            Name = "Bravo",
+            MaxCheckpoints = 5,
+            DelayMs = 200
+        };
+
+        DroneModel charlie = new DroneModel
+        {
+            Name = "Charlie",
+            MaxCheckpoints = 5,
+            DelayMs = 700
+        };
+
+        DroneModel delta = new DroneModel
+        {
+            Name = "Delta",
+            MaxCheckpoints = 5,
+            DelayMs = 400
+        };
+
+        Task alphaTask = FlyDroneAsync(alpha);
+        Task bravoTask = FlyDroneAsync(bravo);
+        Task charlieTask = FlyDroneAsync(charlie);
+        Task deltaTask = FlyDroneAsync(delta);
+
+        try
+        {
+            await Task.WhenAll(
+                alphaTask,
+                bravoTask,
+                charlieTask,
+                deltaTask
+        );
+            Console.WriteLine("Alle droner er fardige!");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"En drone feilet: {ex.Message}");
+        }
+    }
+}
